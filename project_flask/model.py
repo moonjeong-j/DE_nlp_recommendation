3cad597b8a5a1fad9d0def21c696ba887f13b842
moonjeong-jang <102526342+moonjeong-jang@users.noreply.github.com>
2023-03-02 13:09:37 +0900

    fix : model fixed

--- project_flask/model.py
+++ project_flask/model.py
@@ -15,9 +15,7 @@ def data_load():
     return df_result
 
 
-
-
-def find_simi_place(df, place_name, price, top_n=1):
+def find_simi_place(df, place_name, price, top_n): #top_n=1
     # from sklearn.feature_extraction.text import CountVectorizer  # 피체 벡터화
     from sklearn.metrics.pairwise import cosine_similarity  # 코사인 유사도
     # from numpy import vectorize
@@ -44,39 +42,36 @@ def find_simi_place(df, place_name, price, top_n=1):
         
     # )
     
-    ##score_sorted import
-    score_sorted = np.load('./similarity_array.npy')
+    #score_sorted import
+    score_sorted = np.load('./project_flask/similarity_array.npy')
+    
+
+    # place_name=keyword 유사도 필터링
+    compare = 0
+    index = 0
+    
+    contains_name = df[df['reviews'].str.contains(place_name)]
+        
+    for i in range(len(contains_name['reviews'])):
+        if contains_name['reviews'].iloc[i].count(place_name) > compare:
+            compare = contains_name['reviews'].iloc[i].count(place_name)
+            index = i
+    contains_name = contains_name.iloc[index:index+1]
+    
+    if contains_name.empty:
+        print('No Data')
+           
+    place_index = contains_name.index.values
+    similar_indexes = score_sorted[place_index, :]  #(top_n)
+    
+    similar_indexes = similar_indexes.reshape(-1)
+    
+    df= df.iloc[similar_indexes]
 
     ## price 필터링
     try:
         df=df[df['price']<= price]
     except Exception as e:
         print('db error:', e)
-    
-
-    ## place_name=keyword 유사도 필터링
-    compare = 0
-    index = 0
-    
-    contains_name = df[df['name'].str.contains(place_name)]
-    contains_name = contains_name.iloc[0:1]
-    
-    if contains_name.empty:
-        print('DataFrame is empty!\nSearch blog review..')
-        contains_name = df[df['reviews'].str.contains(place_name)]
         
-        for i in range(len(contains_name['reviews'])):
-            if contains_name['reviews'].iloc[i].count(place_name) > compare:
-                compare = contains_name['reviews'].iloc[i].count(place_name)
-                index = i
-        contains_name = contains_name.iloc[index:index+1]
-        
-        if contains_name.empty:
-            print('No Data')
-            
-    place_index = contains_name.index.values
-    similar_indexes = score_sorted[place_index, :(top_n)]
-    similar_indexes = similar_indexes.reshape(-1)
-    
-    return df.iloc[similar_indexes]
-
+    return df.iloc[ :top_n, : ]
